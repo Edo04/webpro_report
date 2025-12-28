5fa72082042aa625665089cd939cb8ac024abce3
Edoaru <pompom2006fumi@icloud.com>
2025-12-28 19:30:02 +0900

    変更

--- seventeen/seventeen.js
+++ seventeen/seventeen.js
@@ -7,8 +7,7 @@ app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 
-// SEVENTEENイベントデータ
-// ...existing code...
+
 
 // SEVENTEENイベントデータ
 let items = [
